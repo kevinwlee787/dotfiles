05a2a8edc8efe73d600f458dfcbd8f848e630182
Kevin Lee <88686213+kevinwlee787@users.noreply.github.com>
2026-07-28 15:24:35 -0500

    port nvim/after/queries/cpp/highlights.scm

---
+++ nvim/after/queries/cpp/highlights.scm
@@ -0,0 +1,42 @@
+; extends
+
+(operator_cast) [ "operator" ] @function
+
+(module_name) @module
+
+((module_name) @module.builtin
+  (#eq? @module.builtin "std"))
+
+((namespace_identifier) @module.builtin
+  (#eq? @module.builtin "std"))
+
+(using_declaration
+  (identifier) @module.builtin
+  (#eq? @module.builtin "std"))
+
+(alignas_qualifier (identifier) @type)
+
+[
+ "alignas"
+ "constinit"
+ "consteval"
+ "thread_local"
+] @keyword
+
+[
+ "extern"
+] @keyword.modifier
+
+[
+ "module"
+ "import"
+ "export"
+] @keyword.import
+
+[
+ "inline"
+ "explicit"
+ "override"
+ "final"
+ "virtual"
+] @keyword.function
