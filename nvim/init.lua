f0bd5f66c0e4a606d5dd7e38c01c75e6c39eaa43
Kevin Lee <88686213+kevinwlee787@users.noreply.github.com>
2026-07-28 14:44:42 -0500

    Port nvim/init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,19 @@
+pcall(vim.loader.enable)
+
+require('options')
+require('lazy-bootstrap')
+require('theme')
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'VeryLazy',
+  callback = function()
+    require('commands')
+    require('mappings')
+
+    local fn = vim.fn
+    local local_init = fn.resolve(fn.stdpath('data') .. '/site/init.lua')
+    if fn.filereadable(local_init) > 0 then
+      vim.cmd('luafile ' .. local_init)
+    end
+  end,
+})
