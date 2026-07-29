cf8b1392cdf9f3be6aa4c32cc52e84d7cbdd9660
Kevin Lee <88686213+kevinwlee787@users.noreply.github.com>
2026-07-29 11:30:44 -0500

    Create lazy-bootstrap.lua

---
+++ nvim/lua/lazy-bootstrap.lua
@@ -0,0 +1,42 @@
+local fn = vim.fn
+
+local lazy_repo = 'https://github.com/folke/lazy.nvim.git'
+local install_path = fn.stdpath('data') .. '/lazy/lazy.nvim'
+
+if not vim.loop.fs_stat(install_path) then
+  vim.notify('Installing lazy.nvim')
+  vim.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    '--single-branch',
+    lazy_repo,
+    install_path
+  }, { text = true }):wait()
+end
+vim.opt.runtimepath:prepend(install_path)
+
+local ViewConfig = require('lazy.view.config')
+ViewConfig.keys.profile_filter = '<M-f>'
+ViewConfig.keys.profile_sort = '<M-s>'
+
+require('lazy').setup('plugins', {
+  defaults = { lazy = true },
+  install = { colorscheme = { 'tokyonight', 'habamax' } },
+  --checker = { enabled = true },
+  dev = { path = '~/source', },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        'gzip',
+        'matchit',
+        'matchparen',
+        'netrwPlugin',
+        'tarPlugin',
+        'tohtml',
+        'tutor',
+        'zipPlugin',
+      },
+    },
+  },
+})
