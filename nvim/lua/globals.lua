8af29c710ff9aa5882f44c647fd0a6a07a1cd6d5
Kevin Lee <88686213+kevinwlee787@users.noreply.github.com>
2026-07-29 11:31:12 -0500

    Create globals.lua

---
+++ nvim/lua/globals.lua
@@ -0,0 +1,19 @@
+local M = {
+  native_lsp = true,
+  blink_cmp = true,
+  fzflua = true,
+
+  --theme = 'carbonfox',
+  theme = 'catppuccin',
+  --theme = 'dracula',
+  --theme = 'duskfox',
+  --theme = 'kanagawa',
+  --theme = 'moonfly',
+  --theme = 'nightfox',
+  --theme = 'onedark',
+  --theme = 'retrobox',
+  --theme = 'rose-pine',
+  --theme = 'tokyonight',
+}
+
+return M
