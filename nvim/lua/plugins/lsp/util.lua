24992d6ad86fed5f9f6d914c729ba0c279ac8c00
Kevin Lee <88686213+kevinwlee787@users.noreply.github.com>
2026-07-30 10:57:06 -0500

    Create util.lua

---
+++ nvim/lua/plugins/lsp/util.lua
@@ -0,0 +1,51 @@
+local api = vim.api
+local tokens = vim.lsp.semantic_tokens
+
+local M = {}
+
+M.pending_requests = vim.defaulttable()
+
+function M.track_request(request_event)
+  local requests = M.pending_requests
+
+  local bufnr = request_event.buf
+  local client_id = request_event.data.client_id
+  local request_id = request_event.data.request_id
+  local request = request_event.data.request
+
+  if request.type == 'pending' and
+      not request.method:match('semanticTokens') and
+      not request.method:match('documentSymbol') then
+    requests[bufnr][client_id][request_id] = request
+  elseif vim.tbl_get(requests, bufnr, client_id, request_id) then
+    requests[bufnr][client_id][request_id] = nil
+    if vim.tbl_isempty(requests[bufnr][client_id]) then
+      requests[bufnr][client_id] = nil
+    end
+    if vim.tbl_isempty(requests[bufnr]) then
+      requests[bufnr] = nil
+    end
+  end
+end
+
+function M.cancel_pending_requests(bufnr)
+  local requests = M.pending_requests
+  if not rawget(requests, bufnr) then
+    return
+  end
+  for client_id, reqs in pairs(requests[bufnr]) do
+    local client = vim.lsp.get_client_by_id(client_id)
+    if client then
+      for id, _ in pairs(reqs) do
+        client:cancel_request(id)
+      end
+    end
+  end
+end
+
+function M.toggle_tokens(bufnr)
+  bufnr = bufnr or api.nvim_get_current_buf()
+  tokens.enable(not tokens.is_enabled({ bufnr = bufnr }), { bufnr = bufnr })
+end
+
+return M
