935ef7e01cda6b6307c9ab83e78bf1dde9e0eb74
Kevin Lee <88686213+kevinwlee787@users.noreply.github.com>
2026-07-30 10:56:31 -0500

    Create barbecue.lua

---
+++ nvim/lua/plugins/barbecue.lua
@@ -0,0 +1,47 @@
+local M = {
+  'utilyre/barbecue.nvim',
+  event = { 'BufReadPost', 'BufNewFile' },
+  enabled = false,
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+
+    {
+      'SmiteshP/nvim-navic',
+      opts = { lazy_update_context = true, },
+    },
+  },
+}
+
+M.opts = {
+  create_autocmd = false,
+  show_modified = true,
+  show_dirname = false,
+  theme = {
+    basename = { bold = true },
+  },
+}
+
+function M.config(_, opts)
+  local api = vim.api
+
+  require('barbecue').setup(opts)
+  vim.o.laststatus = 3
+  vim.o.winbar = '%#barbecue_basename#%t%X'
+
+  local group = api.nvim_create_augroup('barbecue.updater', {})
+
+  api.nvim_create_autocmd({
+    'WinResized',
+    'BufWinEnter',
+    'CursorHold',
+    'InsertLeave',
+    'BufModifiedSet',
+  }, {
+    group = group,
+    callback = function()
+      require('barbecue.ui').update()
+    end,
+  })
+end
+
+return M
