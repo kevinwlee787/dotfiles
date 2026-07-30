ee05120f784b3d9ea3411c131c996df18f88925b
Kevin Lee <88686213+kevinwlee787@users.noreply.github.com>
2026-07-30 10:43:01 -0500

    Create gitsigns.lua

---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,51 @@
+local M = {
+  'lewis6991/gitsigns.nvim',
+  event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
+}
+
+M.opts = {
+  numhl = true,
+  on_attach = function(bufnr)
+    local gs = require('gitsigns')
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({ ']c', bang = true })
+      else
+        gs.nav_hunk('next')
+      end
+    end)
+
+    map('n', '[c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({ '[c', bang = true })
+      else
+        gs.nav_hunk('prev')
+      end
+    end)
+
+    -- Actions
+    map({ 'n', 'v' }, '<leader>hs', gs.stage_hunk)
+    map({ 'n', 'v' }, '<leader>hr', gs.reset_hunk)
+    map('n', '<leader>hS', gs.stage_buffer)
+    map('n', '<leader>hR', gs.reset_buffer)
+    map('n', '<leader>hp', gs.preview_hunk)
+    map('n', '<leader>hb', function() gs.blame_line { full = true } end)
+    map('n', '<leader>tb', gs.toggle_current_line_blame)
+    map('n', '<leader>hd', gs.diffthis)
+    map('n', '<leader>hD', function() gs.diffthis('~') end)
+    map('n', '<leader>td', gs.toggle_deleted)
+
+    -- Text object
+    map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end
+}
+
+return M
